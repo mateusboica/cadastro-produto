0b1066d5841c82ccd99122d89123b635501fbdd9
Mateus Henrique Almeida Boiça <mateus.henrique@tc.df.gov.br>
2026-04-08 10:52:43 -0300

    alterando arquivo jsx para tsx

---
+++ src/routes/router.tsx
@@ -0,0 +1,12 @@
+import { Route, Routes } from 'react-router-dom'
+import App from '../App'
+import LoginPage from '../LoginPage'
+
+export default function Router() {
+  return (
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/login" element={<LoginPage />} />
+    </Routes>
+  )
+}
