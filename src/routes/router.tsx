b310b2acbff06eda253cb4b79fe899d4b5590600
mateusboica <mateus.ha.boica@gmail.com>
2026-04-14 20:07:55 -0300

    teste 2 erro router

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,15 +1,13 @@
 import { Route, Routes } from 'react-router-dom'
 import App from '../App'
-import LoginPage from '../pages/LoginPage'
 import RegisterPage from '../pages/RegisterPage'
 
 export default function Router() {
   return (
     <Routes>
       <Route path="/" element={<App />} />
-      <Route path="/login" element={<LoginPage />} />
+      <Route path="/login" element={<h1 style={{ color: 'black', backgroundColor: 'white' }}>TESTE FUNCIONANDO</h1>} />
       <Route path="/register" element={<RegisterPage />} />
-      <Route path="*" element={<h1 style={{ color: 'red' }}>A ROTA NÃO FOI ENCONTRADA</h1>} />
     </Routes>
   )
 }
