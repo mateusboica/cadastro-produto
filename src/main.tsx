2c111f612d6d83bcbfd2f793344a3f01ae1d6a2b
mateusboica <mateus.ha.boica@gmail.com>
2026-04-14 20:29:29 -0300

    testando se o main ta rodando

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,6 @@
+console.log("🚀 O ARQUIVO MAIN.JSX CARREGOU!");
+console.log("Variavel de teste:", import.meta.env.VITE_API_URL);
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
