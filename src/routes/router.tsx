672d76f64e182074085e886d801515f04f866df5
unknown <vitor.gama@sempreceub.com>
2026-04-14 14:28:43 -0300

    retirando browser router repetido

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,16 +1,14 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import App from '../App'
 import LoginPage from '../pages/LoginPage'
 import RegisterPage from '../pages/RegisterPage'
 
 export default function Router() {
   return (
-    <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />} />
       <Route path="/login" element={<LoginPage />} />
       <Route path="/register" element={<RegisterPage />} />
     </Routes>
-    </BrowserRouter>
   )
 }
