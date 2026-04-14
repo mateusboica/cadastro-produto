2097b2f70abd96aa09d3ca9ecaed3fea5849fd3c
Mateus Henrique Almeida Boiça <mateus.henrique@tc.df.gov.br>
2026-04-14 10:14:37 -0300

    alguns ajustes no router

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,14 +1,16 @@
-import { Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import App from '../App'
 import LoginPage from '../pages/LoginPage'
 import RegisterPage from '../pages/RegisterPage'
 
 export default function Router() {
   return (
+    <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />} />
       <Route path="/login" element={<LoginPage />} />
       <Route path="/register" element={<RegisterPage />} />
     </Routes>
+    </BrowserRouter>
   )
 }
